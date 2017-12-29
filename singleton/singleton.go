118129e23aaecf8ffe98be088c9bb80450c732e8
U+ <cool-whispers@hotmail.com>
2017-12-29 15:02:52 +0800

    add singleton

---
+++ singleton/singleton.go
@@ -0,0 +1,23 @@
+package Singleton
+
+import "sync"
+
+//Singleton 單體模式
+type Singleton struct{}
+
+var singleton *Singleton
+
+var singletonLock sync.Mutex
+
+//GetInstance 取得實體
+func GetInstance() *Singleton {
+
+	if singleton == nil {
+		singletonLock.Lock()
+		if singleton == nil {
+			singleton = &Singleton{}
+		}
+	}
+
+	return singleton
+}
